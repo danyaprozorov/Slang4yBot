0249439691ee82fede2251c484d91cb3ae80447c
Nikita Andreev <nsandreev_2@edu.hse.ru>
2020-06-06 17:33:39 +0300

    Changed pictures to gifs. Added tracking of selected words

--- bot.py
+++ bot.py
@@ -1,25 +1,26 @@
 import telebot
 import cfg
+import random
 from telebot import types
 
 bot = telebot.TeleBot(cfg.TOKEN)
 
 
 class Words(object):
-    def __init__(self, wrd, trans, diff, topic, img, prog):
+    def __init__(self, wrd, trans, diff, topic, img, prog, prog2):
         self.wrd = wrd
         self.trans = trans
         self.diff = diff
         self.topic = topic
         self.img = img
         self.prog = prog
+        self.prog2 = prog2
 
 
 class user:
     def __init__(self, id, choic):
         self.id = id
         self.choic = choic
-
 
 
 @bot.message_handler(commands=['start'])
@@ -54,14 +55,58 @@ def get_text(message):
         wordss[nikita.choic].prog = 2
         nikita.choic += 1
         choice(message)
+    if message.text == 'Учить слова':
+        learn(message)
+    if message.text == 'Изучаемые слова':
+        learnwords(message)
+#    if message.text == 'Начать изучение':
+#        try:
+#            i = 0
+#            j = 1
+#            while i < 4:
+#                if wordss[i].prog == 1:
+#                    j += 1
+#                i += 1
+#            if j == 1:
 
+
+def learnwords(message):
+    i = 0
+    j = 1
+    while i < 4:
+        if wordss[i].prog == 1:
+            bot.send_message(message.chat.id, str(j) + '. ' + wordss[i].wrd + ' [' + str(wordss[i].prog2) + '/4]')
+            j += 1
+        i += 1
+    if j == 1:
+        bot.send_message(message.chat.id, 'Нет изучемых слов, пополните их в разделе Выбор слов')
+        main_menu(message)
+    else:
+        have(message)
+
+def have(message):
+    markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item_start = types.KeyboardButton('Начать изучение')
+    item_back = types.KeyboardButton('Главное меню')
+    markup_reply.add(item_start, item_back)
+    bot.send_message(message.chat.id, 'Выбирай)', reply_markup=markup_reply)
+
+
+def learn(message):
+    markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item_start = types.KeyboardButton('Начать изучение')
+    item_have = types.KeyboardButton('Изучаемые слова')
+    item_back = types.KeyboardButton('Главное меню')
+    markup_reply.add(item_start, item_have, item_back)
+    bot.send_message(message.chat.id, 'Выбирай)', reply_markup=markup_reply)
 
 
 def choice(message):
     try:
         if wordss[nikita.choic].prog == 0:
-            bot.send_photo(message.chat.id, open(wordss[nikita.choic].img, 'rb'))
-            bot.send_message(message.chat.id, wordss[nikita.choic].wrd + ' (' + wordss[nikita.choic].trans + ') - ' + wordss[nikita.choic].diff)
+            bot.send_animation(message.chat.id, open(wordss[nikita.choic].img, 'rb'))
+            bot.send_message(message.chat.id, wordss[nikita.choic].wrd + ' - ' + wordss[nikita.choic].trans)
+            bot.send_message(message.chat.id, wordss[nikita.choic].diff)
             markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
             item_yes = types.KeyboardButton('Изучить')
             item_no = types.KeyboardButton('Отложить')
@@ -83,7 +128,6 @@ def choice(message):
         main_menu(message)
 
 
-
 def main_menu(message):
     markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item_progress = types.KeyboardButton('Мой прогресс')
@@ -95,15 +139,18 @@ def main_menu(message):
 
 
 wordss = []
+learning = []
 i = 0
 f = open("words1.txt", "r", encoding='utf-8')
 while i < 16:
-    wordss.append(Words(f.readline().strip(), f.readline().strip(), f.readline().strip(), f.readline().strip(), f.readline().strip(), f.readline()))
+    wordss.append(Words(f.readline().strip(), f.readline().strip(), f.readline().strip(), f.readline().strip(),
+                        f.readline().strip(), f.readline(), f.readline()))
     i += 1
-f.close
+f.close()
 i = 0
 while i < 4:
     wordss[i].prog = int(wordss[i].prog)
+    wordss[i].prog2 = int(wordss[i].prog2)
     i += 1
 nikita = user(0, 0)
 bot.polling(none_stop=True)
