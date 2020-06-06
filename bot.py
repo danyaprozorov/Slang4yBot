47269b1fa1c80c9cf141040e269ded7fe5d2deba
Nikita Andreev <nsandreev_2@edu.hse.ru>
2020-06-06 13:39:41 +0300

    Small test database

--- bot.py
+++ bot.py
@@ -5,6 +5,15 @@ from telebot import types
 bot = telebot.TeleBot(cfg.TOKEN)
 
 
+class Words(object):
+    def __init__(self, wrd, trans, diff, topic, img):
+        self.wrd = wrd
+        self.trans = trans
+        self.diff = diff
+        self.topic = topic
+        self.img = img
+
+
 class user:
     def __init__(self, id):
         self.id = id
@@ -24,9 +33,11 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def get_text(message):
     if message.text == 'Hello':
-        bot.send_message(message.chat.id, f'Отлично! рад видеть тебя, {message.from_user.first_name}')
+        bot.send_message(message.chat.id, f'Рад видеть тебя, {message.from_user.first_name}')
         bot.send_message(message.chat.id, 'Снизу меню, с чего начнем?')
         main_menu(message)
+    if message.text == 'Выбор слов':
+
 
 
 def main_menu(message):
@@ -39,7 +50,13 @@ def main_menu(message):
     bot.send_message(message.chat.id, 'Жми на кнопку)', reply_markup=markup_reply)
 
 
-
+wordss = []
+i = 0
+f = open("words1.txt", "r", encoding='utf-8')
+while i < 16:
+    wordss.append(Words(f.readline(), f.readline(), f.readline(), f.readline()., f.readline().strip()))
+    i += 1
+f.close
 
 Nikita = user(0)
 bot.polling(none_stop=True)
